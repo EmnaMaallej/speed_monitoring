3b7ac7df22dc8130e9ad37d8594742b65b29208e
EmnaMaallej <emna.maalej@supcom.tn>
2025-07-22 23:40:32 +0100

    Update server.hpp

--- include/server.hpp
+++ include/server.hpp
@@ -1,1 +1,18 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
 
+#include <vsomeip/vsomeip.hpp>
+
+class Server {
+public:
+    Server();
+    void start();
+    void on_speed_value(const std::shared_ptr<vsomeip::message>&);
+
+private:
+    std::shared_ptr<vsomeip::application> app_;
+    std::string client1_endpoint_ = "127.0.0.1:30000"; // Assume Client1 listens here
+    void process_speed(float speed_kmh);
+};
+
+#endif
