378919d22efd63946fbc5b04b40b9c30e8c321fc
EmnaMaallej <emna.maalej@supcom.tn>
2025-07-22 23:40:01 +0100

    Update client2.hpp

--- include/client2.hpp
+++ include/client2.hpp
@@ -1,1 +1,18 @@
+#ifndef CLIENT2_HPP
+#define CLIENT2_HPP
 
+#include <vsomeip/vsomeip.hpp>
+
+class Client2 {
+public:
+    Client2();
+    void start();
+    void on_speed_request(const std::shared_ptr<vsomeip::message>&);
+
+private:
+    std::shared_ptr<vsomeip::application> app_;
+    std::string client1_endpoint_ = "127.0.0.1:30000"; // Assume Client1 listens here
+    float get_current_speed();
+};
+
+#endif
